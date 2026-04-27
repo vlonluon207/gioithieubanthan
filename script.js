5769722f602165c7bcd36aeba0180c50e59fea2d
vlonluon207 <thangduong557@gmail.com~>
2026-04-28 02:18:23 +0700

    dumamay

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
-const navToggle = document.querySelector(".nav-toggle");
-const siteNav = document.querySelector(".site-nav");
-
-navToggle.addEventListener("click", () => {
-  const isOpen = siteNav.classList.toggle("is-open");
-  navToggle.setAttribute("aria-expanded", String(isOpen));
-});
-
-siteNav.querySelectorAll("a").forEach((link) => {
-  link.addEventListener("click", () => {
-    siteNav.classList.remove("is-open");
-    navToggle.setAttribute("aria-expanded", "false");
+document.querySelectorAll("a[href='#']").forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
   });
 });
